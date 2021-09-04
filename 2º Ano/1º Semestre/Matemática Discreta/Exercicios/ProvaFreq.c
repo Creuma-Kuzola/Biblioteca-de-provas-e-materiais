8102d662e17e185340d9701523b3525e32d0346f
Héber Júlio <46345585+kurogai@users.noreply.github.com>
2021-09-04 22:58:58 +0100

    Melhoria de código
    Correção de algumas funções e pequenos trechos de código

--- 2º Ano/1º Semestre/Matemática Discreta/Exercicios/ProvaFreq.c
+++ 2º Ano/1º Semestre/Matemática Discreta/Exercicios/ProvaFreq.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define TRUE 1
-#define FALSE 0
-
-/*********************PROTÓTIPO DE FUNÇÕES *********************/
+/*********************PROTï¿½TIPO DE FUNï¿½ï¿½ES *********************/
 void imprimirVet(int vet[], int nElem);
 void imprimirStr(char str[]);
 char toUpper(char letra);
@@ -20,89 +17,71 @@ int EhCapicua(int vet[], int i, int f);
 //imprimir numero por numero de um vetor
 void imprimirVet(int vet[], int nElem)
 {
-	int i;
-	for( i =0; i<nElem; ++i)
-		printf(" |%d| ",vet[i]);
+	for(int i = 0; i < nElem; ++i) printf("|%d| ",vet[i]);
 }
 
 //imprimir caracter por caracter da cadeia
 void imprimirStr(char str[])
 {
-	int i;
-	for( i =0; str[i] != '\0'; ++i)
-		printf("%c",str[i]);
+	for(int i = 0; str[i] != '\0'; i++) printf("%c",str[i]);
 }
 
 //converter letra para maiusculo
 char toUpper(char letra)
 {
-	if(letra >= 'a' && letra <= 'z')
-		return letra + ('A' - 'a');
-	return letra;
+  return (letra >= 'a' && letra <= 'z') ? letra + ('A' - 'a') : letra;
 }
 
 //converter letra para minusculo
 char toLower(char letra)
 {
-	if(letra >= 'A' && letra <= 'Z')
-		return letra - ('A' - 'a');
-	return letra;	
+	return (letra >= 'A' && letra <= 'Z') ? letra - ('A' - 'a') : letra;
 }
 
-//verificar de uma letra é uma consoante
+//verificar de uma letra ï¿½ uma consoante
 int verifConsoante(char letra)
 {
-	if(letra != 'a' && letra != 'e' && letra != 'i' && letra != 'o' && letra != 'u' 
+	return (letra != 'a' && letra != 'e' && letra != 'i' && letra != 'o' && letra != 'u' 
 		&& letra != 'A' && letra != 'E' && letra != 'I' && letra != 'O' && letra != 'U' 
-		&& letra != ' ') return TRUE;
-	return FALSE;
+		&& letra != ' ');
 }
 
-//verificar de uma letra é uma vogal
+//verificar de uma letra ï¿½ uma vogal
 int verifVogal(char letra)
 {
-	if(letra == 'a'|| letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u') 
-		return TRUE;
-	return FALSE;
+	return (letra == 'a'|| letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u');
 }
 
 //EXERCICIO 3
 //converter as vogais para Maiusculas e consoantes para minusculas
 void substituir(char str[], int i)
 {
-	if(str[i] != '\0')
-	{
-		if(verifVogal(str[i]) == TRUE) 
-			str[i] = toUpper(str[i]);
-		if(verifConsoante(str[i]) == TRUE) 
-			str[i] = toLower(str[i]);
+	if(str[i]) {
+		if(verifVogal(str[i])) str[i] = toUpper(str[i]);
+		if(verifConsoante(str[i])) str[i] = toLower(str[i]);
 		substituir(str, i+1);
 	}	
 }
 
-//EXERCICIO 5
-//devolver o maior valor e menor valor de um vetor
+//EXERCICIO 5 : devolver o maior valor e menor valor de um vetor
 void MAX_MIN(int vet[], int nElem, int *MAX, int *MIN)
 {
 	nElem--;
-	if(nElem >= 0)
-	{
+	if(nElem >= 0) {
 		if(vet[nElem] > *MAX) *MAX = vet[nElem];
 		if(vet[nElem] < *MIN) *MIN = vet[nElem];
 		MAX_MIN(vet, nElem, MAX, MIN);
 	}
 }
 
-//EXERCICIO 1
-//numeros pares no inicio e impares no fim
+//EXERCICIO 1 : numeros pares no inicio e impares no fim
 void organizar(int vet[], int nElem)
 {
 	int aux, i, j;
 	for( i = 0; i < nElem; ++i)
 		if((vet[i]%2) == 0)
 			for(j = 0; j < i; ++j)
-				if((vet[j]%2) != 0)
-				{
+				if((vet[j]%2) != 0){
 					aux = vet[j];
 					vet[j] = vet[i];
 					vet[i] = aux;
@@ -110,28 +89,19 @@ void organizar(int vet[], int nElem)
 }
 
 //EXERCICIO 4
-//
 int EhCapicua(int vet[], int i, int f)
 {
 	int valor1, valor2, k;
-	
-	if(i == f)
-		return TRUE;
-	else 
-	{
-		if(vet[i] != vet[f])
-			return FALSE;
-		k = (i + f)/2;
-		valor1 = EhCapicua(vet, i, k);
-		valor2 = EhCapicua(vet, k+1, f);
-	}
-//	return FALSE;
+	if(i == f) return 1;
+  if(vet[i] != vet[f]) return 0;
+  k = (i + f)/2;
+  valor1 = EhCapicua(vet, i, k);
+  valor2 = EhCapicua(vet, k+1, f);
 }
-
 
 int main()
 {
-	char str[] = {'e','D','S','o','N', ' ','P','a','u','L','O','\0'};
+	char str[] = {'e','D','S','o','N', ' ','P','a','u','L','O'};
 	int vet[] = {2,5,4,11,5,8,7,51,20,15,12,19,10,14,21,9};
 	int vetCapicua[] = {5,3,1,3,5};
 	int nElem = 16;
@@ -143,10 +113,8 @@ int main()
 	imprimirVet(vet, nElem);
 	
 	printf("\n\n");
-	
 	MAX_MIN(vet, nElem, &MAX , &MIN);
 	printf("\nMAX: %d - MIN: %d", MAX, MIN);
-
 	printf("\n\n");
 	
 	imprimirStr(str);
@@ -155,12 +123,11 @@ int main()
 	imprimirStr(str);
 	
 	printf("\n\n");
-	
-	if(EhCapicua(vetCapicua, 0, 4)  == TRUE)
+	if(EhCapicua(vetCapicua, 0, 4))
 		printf("\nEh Capicua");
 	else printf("\nNao Eh Capicua");
 
 	printf("\n\n");
-	system("PAUSE");
+	system("pause");
 	return 0;	
 }
